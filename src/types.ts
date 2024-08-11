613429ad727bd860365926564c746130f4c2abfa
Arnab Baruah <baruaharnab769@gmail.com>
2024-08-11 17:55:41 +0530

    added Response type

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,11 @@
 export type CustomerType = {
   id: number;
   name: string;
-  image: string,
+  image: string;
   address: string;
   about: string;
+};
+
+export type Response = {
+  message: string[];
 };
